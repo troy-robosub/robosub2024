0dec81abc0a34cd24ea871f49d30db8952dae924
Derek Peng <90730846+Cybertron51@users.noreply.github.com>
2024-08-04 03:44:38 -0700

    Create camera.py

---
+++ sub/vision/camera.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python 
+
+# Krishna and Kaileo, with help of Tim
+
+import std_msgs
+from std_msgs.msg import String
+import cv2 
+from ultralytics import YOLO
+import numpy as np
+import rospy 
+
+model = YOLO("best50.pt")
+cap = cv2.VideoCapture(-1)
+classes = {0: 'bin', 1: 'bin_bleue', 2: 'bin_rouge', 3: 'buoy', 4: 'gate', 5: 'gate_bleue', 6: 'gate_rouge', 7: 'path', 8: 'torpille', 9: 'torpille_target'}
+frame_size = (640, 480)
+frame_center = (frame_size[0] / 2, frame_size[1] / 2)
+
+def find_angle_to_object(x1,y1,x2,y2):
+    # W KRISHNA WHO FOUND TIM'S CODE 
+    frame_center = (320, 320)
+    object_center = ((x1 + x2) // 2), ((y1 + y2 )// 2)
+    
+    angle = np.degrees(np.arctan2(object_center[1] - frame_center[1], 
+                                  object_center[0] - frame_center[0]))
+    return(angle)
+
+def inference(): 
+    pub = rospy.Publisher('/inference_results', String, queue_size=10) #try changing the 'chatter' part
+    rospy.init_node('inference', anonymous = True)
+    rate = rospy.Rate(10)
+    
+    while (True):
+        success, frame = cap.read()
+
+        if success:
+            # run inference on one frame
+            results = model(source=frame, conf=0.3, show = True)
+
+            # get data from inference in ultralytics.engine.results.boxes object
+            # contains ordered lists with detection data
+            boxes = results[0].boxes
+        
+            out = str(len(boxes.cls))
+
+            # for each detected object in the current frame
+            for i in range(len(boxes.cls)):
+            # data will be outputted through ros topic
+                left = float(boxes.xyxy[i][0])
+                top = float(boxes.xyxy[i][1])
+                right = float(boxes.xyxy[i][2])
+                bottom = float(boxes.xyxy[i][3])
+                
+                out += "{" + classes[int(boxes.cls[i])] + " " + str(left) + " " + str(top) + " " + str(right) + " " + str(bottom) + "} "
+                
+                rospy.loginfo(out)
+                pub.publish(out)
+                rate.sleep()
+                
+            # format
+            # numberofobjects {object1class object1top object1left object1bottom object1right object1anglefromcenter} {object1class object1top object1left object1bottom object1right object1anglefromcenter}
+
+
+        # shows the stuff
+        #cv2.imshow("YOLOv8 Inference (live)", results[0].plot())
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+        else:
+            break
+
+while (True): 
+    if __name__ == '__main__':
+        try:
+            inference()
+        except rospy.ROSInterruptException:
+            pass

759c44882d38c8029a037e2489b868d6828fe7a3
brucdeng <122509493+brucdeng@users.noreply.github.com>
2024-08-04 20:26:59 -0700

    Update movement_control.py

--- sub/motion/movement_control.py
+++ sub/motion/movement_control.py
@@ -1,10 +1,57 @@
 import rospy
-from mavros_msgs.srv import CommandBool, SetMode
+import mavros_msgs.msg
+import mavros_msgs.srv
+import rospy
+from std_msgs.msg import Float64, Float32MultiArray
 from arm import arm_submarine
 from disarm import disarm_submarine
 ## using mavros to apply major motion
 arm_submarine()
 disarm_submarine()
+
+pub_thrusters = rospy.Publisher("auv/devices/thrusters", mavros_msgs.msg.OverrideRCIn, queue_size=10)
+
+def movement(
+        self,
+        yaw=None,
+        forward=None,
+        lateral=None,
+        pitch=None,
+        roll=None,
+        vertical=0,
+        **kwargs,
+    ):
+        """
+        Move the robot in a given direction, by directly changing the PWM value of each thruster. This does not take input from the DVL.
+        This is a non-blocking function.
+        Inputs are between -5 and 5
+
+        Args:
+            yaw (float): Power for the yaw maneuver
+            forward (float): Power to move forward
+            lateral (float): Power for moving laterally (negative one way (less than 1500), positive the other way (more than 1500))
+            pitch (float): Power for the pitch maneuver
+            roll (float): Power for the roll maneuver
+            vertical (float): Distance to change the depth by
+
+        # TODO Handle timeout of the pixhawk
+        """
+
+        pwm = mavros_msgs.msg.OverrideRCIn()
+
+        # Calculate PWM values
+        channels = [1500] * 18
+        # channels[2] = int((vertical * 80) + 1500) if vertical else 1500
+        channels[3] = int((yaw * 80) + 1500) if yaw else 1500
+        channels[4] = int((forward * 80) + 1500) if forward else 1500
+        channels[5] = int((lateral * 80) + 1500) if lateral else 1500
+        channels[6] = int((pitch * 80) + 1500) if pitch else 1500
+        channels[7] = int((roll * 80) + 1500) if roll else 1500
+        pwm.channels = channels
+
+        # Publish PWMs to /auv/devices/thrusters
+        print(f"[INFO] Channels sent to pixhawk = {pwm}")
+        pub_thrusters.publish(pwm)
 
 # https://www.ardusub.com/developers/rc-input-and-output.html
 

cb87999f8a9551ad89e9f3702ee73424984a1b08
brucdeng <122509493+brucdeng@users.noreply.github.com>
2024-08-06 09:24:56 -0700

    Update run.py

--- sub/run.py
+++ sub/run.py
@@ -2,4 +2,11 @@ from motion import arm_submarine, disarm_submarine
 from dvl import send_dvl_command
 
 def main():
+  arm_submarine.arm_submarine()
+  send_dvl_command.main("calibrate_gyro")
+  send_dvl_command.main("reset_dead_reckoning")
+  subprocess.Popen(['python3', '/dvl/dvl_tcp_parser.py', 'dead_reckoning', '-i', '192.168.137.101'])
+
+
+  disarm_submarine.disarm_submarine()
   

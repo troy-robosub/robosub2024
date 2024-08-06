b01b304e71b966e042dbae06af9db585c652a228
brucdeng <122509493+brucdeng@users.noreply.github.com>
2024-08-06 09:35:24 -0700

    Create dvl_publisher.py

---
+++ sub/dvl/dvl_publisher.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+import rospy
+import os
+import json
+from std_msgs.msg import String
+import subprocess
+
+subprocess.Popen(['python3', '/dvl/dvl_tcp_parser.py', 'dead_reckoning', '-i', '192.168.137.101'])
+nodeName='messagepublisher'
+topicName='information'
+#initializing node
+rospy.init_node(nodeName, anonymous=True)
+#publishing to topic
+publisher1=rospy.Publisher(topicName,String,queue_size=5)
+
+#1Hz means 1 message per second
+ratePublisher=rospy.Rate(1)
+
+while not rospy.is_shutdown():
+	try:
+		with open('out.json','r') as f:
+			data = json.load(f)
+			data_str=json.dumps(data)
+	except (json.JSONDecodeError):
+		data_str="gagagag"
+	rospy.loginfo(data_str)
+	#publishing the message
+	publisher1.publish(data_str)
+	
+	ratePublisher.sleep()
+	

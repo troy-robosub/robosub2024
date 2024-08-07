75f826364b72df846ae7a31892ed87690ce9b976
brucdeng <122509493+brucdeng@users.noreply.github.com>
2024-08-06 22:44:26 -0700

    Update run.py

--- sub/run.py
+++ sub/run.py
@@ -1,6 +1,7 @@
 from motion import arm_submarine, disarm_submarine, movement_control
 from dvl import send_dvl_command
 import json
+import rospy
 import subprocess
 
 def main():
@@ -9,15 +10,18 @@ def main():
   send_dvl_command.main("calibrate_gyro")
   send_dvl_command.main("reset_dead_reckoning")
   subprocess.Popen('roscore', shell=True)
-  subprocess.Popen(['python3', 'dvl/dvl_tcp_parser.py', 'dead_reckoning', '-i', '192.168.137.101'])
-  #subprocess.Popen(['python3', 'dvl/dvl_publisher.py'])
+  #subprocess.Popen(['python3', 'dvl/dvl_tcp_parser.py', 'dead_reckoning', '-i', '192.168.137.101'])
+  subprocess.Popen(['python3', 'dvl/dvl_publisher.py'])
 
-while not rospy.is_shutdown():
-    with open('/dvl/out.json', 'r') as f:
+# if rospy doesn't work try using dvl_tcp_parser.py directly
+  while not rospy.is_shutdown():
+    with open('dvl/out.json', 'r') as f:
     	data = json.load(f)
     if data["x"] > 9.5:
     	break
-    # fill movement code here
+    movement_control.movement(forward=1)
 
-disarm_submarine.disarm_submarine()
+  disarm_submarine.disarm_submarine()
+
+main()
   

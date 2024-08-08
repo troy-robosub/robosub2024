f696e9d7f306076a0aefd09dcbeedddbd786d22c
brucdeng <122509493+brucdeng@users.noreply.github.com>
2024-08-08 09:41:46 -0700

    Update pymavrc.py

--- sub/pymavrc.py
+++ sub/pymavrc.py
@@ -55,6 +55,77 @@ def send_rc(rcin1=65535, rcin2=65535, rcin3=65535, rcin4=65535,
             *rc_channel_values
         )
 
+#check for heading in Qgroundcontrol
+def get_heading():
+    heading = 0
+    while True:
+        msg = master.recv_match()
+        if not msg:
+            continue
+        if msg.get_type() == 'VFR_HUD':
+            data = str(msg)
+            try:
+                data = data.split(":")
+                heading = data[3].split(",")[0]
+            except:
+                print('')
+
+            heading = float(heading)
+            break
+
+    return heading
+
+def forward(pwm=1600, duration): #default pwm is 1600, but can definitely adjust, and duration is in seconds
+    send_rc(forward=pwm)
+    for i in range(duration/0.05):
+        send_rc()
+        time.sleep(0.05)
+    clear_motion()
+
+# def turnHeading(heading):
+#     #hold altitude
+#     mode = 'ALT_HOLD'
+#     mode_id = master.mode_mapping()[mode]
+#     master.mav.set_mode_send(
+#         master.target_system,
+#         mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+#         mode_id)
+#     #get the current heading
+#     start_heading = get_heading()
+#     #calculate the difference in headings
+#     angle = abs(start_heading - heading) #derek you put abs bruh it cant be negative we changed it to float were lowkey goated
+#     #if the angle is negative, rotate clockwise (increase heading)
+#     if (start_heading > heading):
+#         while (start_heading > heading) {
+
+def rotateClockwise(degrees):
+    #hold altitude and send message
+    set_mode("ALT_HOLD")
+    print("start heading: " + str(get_heading()))
+    
+    #the original heading of the vehicle.
+    #what is heading? the compass direction in which the craft's nose is pointing
+    #https://mavlink.io/en/messages/common.html#ATTITUDE
+    start_heading = get_heading()
+    new_heading = (start_heading + degrees) % 360
+    #run this 10000000 times
+    for i in range(10000000):
+        print(i) # does it only run this loop once?
+        #get current heading, usually after small shift in yaw
+        current_heading = get_heading()
+        #calculate the difference in rotation by degrees
+        #rotate clockwise, no thrust
+        send_rc(yaw=1600)
+        print("current heading: " + str(get_heading()))
+        # if the desired degrees rotated
+        # is greater than the desired rotation (within 4%), stop
+        if current_heading > 0.9 * new_heading and  current_heading < 1.1 * new_heading:
+            break
+    #print the rotation reached
+        print("ROTATED: ", current_heading)
+    #hold altitude
+    clear_motion()
+
 def status_loop(duration, delay=0.05):
         ''' Loop for 'duration', with 'delay' between iterations. [s]
         Useful for debugging.
@@ -64,7 +135,7 @@ def status_loop(duration, delay=0.05):
         while time.time() - start < duration:
             time.sleep(delay)
 
-def clear_motion(self, stopped_pwm=1500):
+def clear_motion(stopped_pwm=1500):
         ''' Sets all 6 motion direction RC inputs to 'stopped_pwm'. '''
         print("running clear_motion")
         send_rc(*[stopped_pwm]*6)
@@ -81,17 +152,7 @@ print("<<<<<<CONNECTION ESTABLISHED>>>>>>")
 # for arming
 master.arducopter_arm()
 
-set_mode("STABILIZE")
-
-send_rc(throttle=1600)
-
-status_loop(2.0)
-
-print("downwards completed")
-
-send_rc(forward=1600)
-
-status_loop(4.0)
+turnClockwise(90)
 
 clear_motion()
 master.arducopter_disarm()

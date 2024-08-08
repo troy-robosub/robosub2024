590c8daf1a77fcba341cf26f30b0a7c910e17428
brucdeng <122509493+brucdeng@users.noreply.github.com>
2024-08-08 10:16:46 -0700

    Update run.py

--- sub/run.py
+++ sub/run.py
@@ -17,30 +17,37 @@ import math
 
 
 def pmove(distance): #distance in meters
-  arm.arm_submarine()
+  #arm.arm_submarine()
   # dvl assisted moving
-  DVL_IP = '192.168.137.101'  # Update this with your DVL's IP address
-  DVL_PORT = 16171
-  # Connect to the DVL
-  dvl_socket = send_dvl_command.connect_to_dvl(DVL_IP, DVL_PORT)
-  if not dvl_socket:
-      print("Failed to connect to DVL")
-      return
-  
-  send_dvl_command.send_command(dvl_socket, "calibrate_gyro")
-  send_dvl_command.send_command(dvl_socket, "reset_dead_reckoning")
+  send_dvl_command.main("calibrate_gyro")
+  send_dvl_command.main("reset_dead_reckoning")
   #subprocess.Popen(['python3', 'dvl/dvl_tcp_parser.py', 'dead_reckoning', '-i', '192.168.137.101'])
   #subprocess.Popen(['python3', 'dvl/dvl_publisher.py'])
 
   # if rospy doesn't work try using dvl_tcp_parser.py directly
-  while True:
-    response_json = send_dvl_command.send_command(dvl_socket, "dead_reckoning")
-    print(response_json)
-    print("i got x value: %s" % str(response_json["x"]))
-    time.sleep(1)
+  listener()
 
-  disarm.disarm_submarine()
+  #disarm.disarm_submarine()
 
+def callback(data):
+    #convert data to a JSON entry
+    json_data = json.loads(data.data)
+    if float(json_data["x"]) > .238:
+    	print("stop moving forward")
+    	#code to stop moving forward
+    else:
+    	print("moving forward")
+    	#code to start moving forward
+
+def listener():
+    # Initialize the ROS node
+    rospy.init_node('exlistener', anonymous=True)
+    
+    # Subscribe to the topic
+    rospy.Subscriber("information", String, callback)
+    
+    # Keep the script running
+    rospy.spin()
 pmove(1)
 
   

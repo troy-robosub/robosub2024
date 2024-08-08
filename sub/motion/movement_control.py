b5b48634ddd3d2e77273adfc578f2a57c7ed1333
Ryan Zhou <ryanzhou1224@gmail.com>
2024-08-08 14:41:29 -0700

    added arbitary function on orientation in relation to vision.

--- sub/motion/movement_control.py
+++ sub/motion/movement_control.py
@@ -3,11 +3,46 @@ import mavros_msgs.msg
 import mavros_msgs.srv
 import rospy
 from std_msgs.msg import Float64, Float32MultiArray
+import camera_ros.py
+
 
 ## using mavros to apply major motion
 
 rospy.init_node('move')
 pub_thrusters = rospy.Publisher("/mavros/rc/override", mavros_msgs.msg.OverrideRCIn, queue_size=10)
+# horizontal degrees view is the point where we make the anlge off of. 
+horizontal_degrees_view = 
+def find_angle_to_object(x1,y1,x2,y2):
+    # W KRISHNA WHO FOUND TIM'S CODE 
+    object_center = ((x1 + x2) // 2), ((y1 + y2 )// 2)
+    hori_angle = (x2-x1)/1920 * horizontal_degrees_view
+    amtx = 500 - object_center
+    return(hori_angle)
+newangle = find_angle_to_object(left,top,right,bottom)
+
+'''
+def obj_distance (x1,x2):
+    target = (x1+x2)/2
+    return (target)
+x = obj_distance (left,right)
+target_difference = 500 - |x|
+while (target_difference > 20):
+    if obj_distance > 500: 
+        # need to go right
+        make the left motors have higher power than right. 
+        make 
+        channels[4] = int 1100
+    else: 
+        # need to go left, elif can be used too. 
+        make the right motors have higher power than left. 
+        channels[4] = int 1900
+
+# publishes the pwm to make the motors move. 
+#in the conditional loop change the yaw (channel 4). 1000-1100 is left, 2000-1900 is right. 
+print(f"[INFO] Channels sent to pixhawk = {pwm}")
+        pub_thrusters.publish(pwm)
+
+'''
 
 def movement(
         yaw=None,
@@ -49,3 +84,57 @@ def movement(
 # https://www.ardusub.com/developers/rc-input-and-output.html
 
 # https://github.com/InspirationRobotics/robosub_2024/blob/dac720839070f41935d3b328c7454b22f64cfa0c/auv/motion/robot_control.py#L2
+
+
+
+
+
+"""
+    def send_rc(self, rcin1=65535, rcin2=65535, rcin3=65535, rcin4=65535,
+                rcin5=65535, rcin6=65535, rcin7=65535, rcin8=65535,
+                rcin9=65535, rcin10=65535, rcin11=65535, rcin12=65535,
+                rcin13=65535, rcin14=65535, rcin15=65535, rcin16=65535,
+                rcin17=65535, rcin18=65535, *, # keyword-only from here
+                pitch=None, roll=None, throttle=None, yaw=None, forward=None,
+                lateral=None, camera_pan=None, camera_tilt=None, lights1=None,
+                lights2=None, video_switch=None):
+        ''' Sets all 18 rc channels as specified.
+        Values should be between 1100-1900, or left as 65535 to ignore.
+        Can specify values:
+            positionally,
+            or with rcinX (X=1-18),
+            or with default RC Input channel mapping names
+              -> see https://ardusub.com/developers/rc-input-and-output.html
+        It's possible to mix and match specifier types (although generally
+          not recommended). Default channel mapping names override positional
+          or rcinX specifiers.
+        '''
+        rc_channel_values = (
+            pitch        or rcin1,
+            roll         or rcin2,
+            throttle     or rcin3,
+            yaw          or rcin4,
+            forward      or rcin5,
+            lateral      or rcin6,
+            camera_pan   or rcin7,
+            camera_tilt  or rcin8,
+            lights1      or rcin9,
+            lights2      or rcin10,
+            video_switch or rcin11,
+            rcin12, rcin13, rcin14, rcin15, rcin16, rcin17, rcin18
+        )
+        logging.info(f'send_rc')
+        logging.debug(rc_channel_values)
+        self.mav.rc_channels_override_send(
+            *self.target,
+            *rc_channel_values
+        )
+"""
+
+
+
+
+# code for issue number 4: angle turning task. 
+# desciprtion: use the data collected in sub/vision/camera_ros.py and utilize the angle return function. 
+# Then, write code to minimize the angle of that through turning. Write this code for special turning within 
+# movement_control.py. For the turning command, just put a placeholder for now.

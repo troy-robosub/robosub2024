8dae77ab7b853bfa4a72a6d5c293df321b87fbc0
brucdeng <122509493+brucdeng@users.noreply.github.com>
2024-08-08 07:41:05 -0700

    Update run.py
    Instead of using janky dvl publisher scripts, im trying to use send_dvl_command.py to get dead_reckoning. Seems more versatile and easier to use

--- sub/run.py
+++ sub/run.py
@@ -1,6 +1,8 @@
-from motion import arm_submarine, disarm_submarine, movement_control
+from motion import arm, disarm, movement_control
 from dvl import send_dvl_command
 import json
+import socket
+import argparse
 import rospy
 import subprocess
 
@@ -15,24 +17,29 @@ import math
 
 
 def pmove(distance): #distance in meters
-  arm_submarine.arm_submarine()
+  arm.arm_submarine()
   # dvl assisted moving
-  send_dvl_command.main("calibrate_gyro")
-  send_dvl_command.main("reset_dead_reckoning")
-  subprocess.Popen('roscore', shell=True)
+  DVL_IP = '192.168.137.101'  # Update this with your DVL's IP address
+  DVL_PORT = 16171
+  # Connect to the DVL
+  dvl_socket = connect_to_dvl(DVL_IP, DVL_PORT)
+  if not dvl_socket:
+      print("Failed to connect to DVL")
+      return
+  
+  send_dvl_command.send_command(dvl_socket, "calibrate_gyro")
+  send_dvl_command.send_command(dvl_socket, "reset_dead_reckoning")
   #subprocess.Popen(['python3', 'dvl/dvl_tcp_parser.py', 'dead_reckoning', '-i', '192.168.137.101'])
-  subprocess.Popen(['python3', 'dvl/dvl_publisher.py'])
+  #subprocess.Popen(['python3', 'dvl/dvl_publisher.py'])
 
   # if rospy doesn't work try using dvl_tcp_parser.py directly
-  while not rospy.is_shutdown():
-    with open('dvl/out.json', 'r') as f:
-      data = json.load(f)
-    if data["x"] > distance-0.25:
-      break
-    # change things in movement function as needed
-    movement_control.movement(forward=1)
+  while True:
+    response_json = send_dvl_command.send_command(dvl_socket, "dead_reckoning")
+    print(response_json)
+    print("i got x value: %s" % str(response_json["x"]))
+    time.sleep(1)
 
-  disarm_submarine.disarm_submarine()
+  disarm.disarm_submarine()
 
 pmove(1)
 

6e1e04c9e5fce16ed22b92c3b26e94fd8814641c
brucdeng <122509493+brucdeng@users.noreply.github.com>
2024-08-08 21:03:16 -0700

    Update dvl_publisher.py
    Moved everything into a callable function, stopped initializing node

--- sub/dvl/dvl_publisher.py
+++ sub/dvl/dvl_publisher.py
@@ -4,27 +4,25 @@ import os
 import json
 from std_msgs.msg import String
 import subprocess
-
-subprocess.Popen(['python3', 'dvl_tcp_parser.py', 'dead_reckoning', '-i', '192.168.137.101'])
-topicName='information'
-#initializing node
-rospy.init_node('dead_reckoning', anonymous=True)
-#publishing to topic
-publisher1=rospy.Publisher(topicName,String,queue_size=5)
-
-#1Hz means 1 message per second
-ratePublisher=rospy.Rate(1)
-
-while not rospy.is_shutdown():
-	try:
-		with open('out.json','r') as f:
-			data = json.load(f)
-			data_str=json.dumps(data)
-	except (json.JSONDecodeError):
-		data_str="JSON Decode Error!"
-	rospy.loginfo(data_str)
-	#publishing the message
-	publisher1.publish(data_str)
+def dvl_parse():
+	subprocess.Popen(['python3', 'dvl_tcp_parser.py', 'dead_reckoning', '-i', '192.168.137.101'])
+	topicName='information'
+	#publishing to topic
+	publisher1=rospy.Publisher(topicName,String,queue_size=5)
 	
-	ratePublisher.sleep()
+	#1Hz means 1 message per second
+	ratePublisher=rospy.Rate(1)
+	
+	while not rospy.is_shutdown():
+		try:
+			with open('out.json','r') as f:
+				data = json.load(f)
+				data_str=json.dumps(data)
+		except (json.JSONDecodeError):
+			data_str="JSON Decode Error!"
+		rospy.loginfo(data_str)
+		#publishing the message
+		publisher1.publish(data_str)
+		
+		ratePublisher.sleep()
 	
